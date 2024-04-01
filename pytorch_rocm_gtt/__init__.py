8bf21077ce8898675f997e81e7b4b3ef84215a56
Thiago F Pappacena <pappacena@gmail.com>
2024-04-01 20:40:09 -0300

    Build at runtime if so file doesnt exist

--- pytorch_rocm_gtt/__init__.py
+++ pytorch_rocm_gtt/__init__.py
@@ -1,9 +1,34 @@
+import os
+import subprocess
+
+import torch
+
+
+HERE = os.path.dirname(__file__)
+SO_FILE_NAME = "gttalloc.so"
+SOURCE_FILE_NAME = "gttalloc.c"
+
+
 def patch():
-    import torch
+    compile()
 
     new_alloc = torch.cuda.memory.CUDAPluggableAllocator(
-        "gttalloc.so",
+        os.path.join(HERE, SO_FILE_NAME),
         "gtt_alloc",
         "gtt_free",
     )
     torch.cuda.memory.change_current_allocator(new_alloc)
+
+
+def compile():
+    if os.path.exists(os.path.join(HERE, "gttalloc.so")):
+        return
+
+    subprocess.run([
+        "hipcc",
+        os.path.join(HERE, SOURCE_FILE_NAME),
+        "-o",
+        os.path.join(HERE, SO_FILE_NAME),
+        "-shared",
+        "-fPIC",
+    ], check=True)
